f8c8ab9a2add21ee73aa4679fd57cede79029b03
davlatrich <94046877+davlatrich@users.noreply.github.com>
2024-11-02 21:16:52 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,36 @@
+from dotenv import load_dotenv
+
 from bot import start_bot
 
-if __name__ == '__main__':
-    start_bot()
+import os
+
+
+
+load_dotenv()  # Загружает переменные окружения из файла .env
+
+
+
+# После этого должна быть проверка переменной TOKEN
+
+token = os.getenv("TOKEN")
+
+if token is None:
+
+    raise ValueError("Токен не задан или не загружен из .env")
+
+
+
+# Использование токена
+
+from aiogram import Bot, Dispatcher
+
+
+
+def start_bot():
+
+    bot = Bot(token=token, parse_mode='HTML')
+
+    dp = Dispatcher(bot)
+
+    # Здесь код для запуска бота...
+
